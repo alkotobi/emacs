a9808c40bb2aed906bb02135e70ca9ffaa8e21fb
merhab <merhab@gmail.com>
2021-10-09 13:41:06 +0100

    org-roam

---
+++ elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "3068ea834ff17bd5d2e8438c1a8d3a6331d10f62")
